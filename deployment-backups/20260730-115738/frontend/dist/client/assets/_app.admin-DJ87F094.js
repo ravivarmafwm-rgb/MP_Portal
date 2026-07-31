c698fc6654411fc2e29a47041c1295c1af6bd6ae
ravivarmafwm-rgb <ravivarmafwm@gmail.com>
2026-07-31 11:25:43 +0530

    Delete deployment-backups/20260730-115738 directory

--- deployment-backups/20260730-115738/frontend/dist/client/assets/_app.admin-DJ87F094.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./useRouter-C7pTCAcE.js";import{t}from"./MpCommandCenter-CmP-BcH6.js";import{t as n}from"./RoleGuard-kXv2xixG.js";var r=e();function i(){return(0,r.jsx)(n,{route:`/admin`,children:(0,r.jsx)(t,{title:`Admin Command Center`})})}export{i as component};
--- deployment-backups/20260730-115738/frontend/dist/client/assets/_app.citizens.index-CknAcegK.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./useRouter-C7pTCAcE.js";import{t}from"./useNavigate-DsyqLLlh.js";var n=e(),r=()=>(0,n.jsx)(t,{to:`/citizens/list`,replace:!0});export{r as component};
